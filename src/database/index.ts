b9d3147d3254a8c3b5a2d3a40fb0e9f937c36528
TheLDB <ldb@erikboles.com>
2023-04-17 03:32:09 -0600

    fix database connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,18 @@
 import pg from 'pg';
 import { Kysely, PostgresDialect } from 'kysely';
 import { Database } from './models';
+import envProvider from '../providers/EnvProvider';
 
 const { Pool } = pg;
 
 const db = new Kysely<Database>({
   dialect: new PostgresDialect({
     pool: new Pool({
-      host: 'localhost',
-      database: 'portals',
-      user: 'postgres',
+      host: envProvider.POSTGRES_HOST,
+      user: envProvider.POSTGRES_USERNAME,
+      password: envProvider.POSTGRES_PASSWORD,
+      database: envProvider.POSTGRES_DB,
+      port: envProvider.POSTGRES_PORT,
     }),
   }),
 });
